38b48235b1bf826108e8e473bc4be28804339c4a
KrankerApfel <t.rakoto76@gmail.com>
2025-04-16 20:16:43 +0200

    deploy docs

---
+++ docs/_nuxt/CwTGu7jr.js
@@ -0,0 +1,1 @@
+import{_ as c,c as e,o as n}from"./COvOLQp1.js";const o={};function r(t,a){return n(),e("h1",null,"Accueil")}const _=c(o,[["render",r]]);export{_ as default};
---
+++ docs/_nuxt/_uJZoLps.js
@@ -0,0 +1,1 @@
+import{_ as o,c as e,o as c}from"./COvOLQp1.js";const r={};function n(s,t){return c(),e("h1",null,"A propos")}const _=o(r,[["render",n]]);export{_ as default};
